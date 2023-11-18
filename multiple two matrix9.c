306cba9b7aa4222d4145cd260685bfe7b8bc5c2d
SaranyaLL <112744611+SaranyaLL@users.noreply.github.com>
2023-11-18 09:15:32 +0530

    Create multiple two matrix9.c

---
+++ multiple two matrix9.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main() 
+{
+    int a[10][10], b[10][10], c[10][10], n, i, j, k;
+    printf("Enter the value of N (N <= 10): ");
+    scanf("%d", & n);
+    printf("Enter the elements of Matrix-A: \n");
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++) 
+		{
+            scanf("%d", & a[i][j]);
+        }
+    }
+    printf("Enter the elements of Matrix-B: \n");
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++) 
+		{
+            scanf("%d", & b[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++) 
+		{
+            c[i][j] = 0;
+            for (k = 0; k < n; k++) 
+			{
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    printf("The product of the two matrices is: \n");
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++) 
+		{
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+ OUTPUT:
+ Enter the value of N (N <= 10): 3
+Enter the elements of Matrix-A:
+9 8 7
+6 5 4
+3 2 1
+Enter the elements of Matrix-B:
+1 2 3
+4 5 6
+7 8 9
+The product of the two matrices is:
+90      114     138
+54      69      84
+18      24      30
